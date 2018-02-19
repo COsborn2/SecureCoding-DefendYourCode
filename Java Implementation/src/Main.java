98b8dcd289bdd18decd985fe368d80db7fad62a7
Jordan Caraway <35328350+JordanCaraway@users.noreply.github.com>
2018-02-18 16:57:16 -0800

    Add files via upload

--- Java Implementation/src/Main.java
+++ Java Implementation/src/Main.java
@@ -1,11 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.regex.Pattern;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-import com.sun.media.sound.InvalidDataException;
 
 /**
  * @author Cameron Osborn
@@ -38,24 +34,43 @@ import com.sun.media.sound.InvalidDataException;
 public class Main {
 
     public static void main(String[] args) {
-        File inputFile;
-        File outputFile;
+        File inputFile = null;
+        File outputFile = null;
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+        Scanner kin = new Scanner(System.in);
         System.out.println(System.getProperty("user.dir")+"\\Main.java");
-        /**
+        /*
+
+         //----------------------------------------------------------------Actual method calls------------------------------
+
          String firstName = getFirstName(buf);
          String lastName = getLastName(buf);
 
          int int1 = getIntValue(buf);
          int int2 = getIntValue(buf);
 
+         inputFile = new File(getInFile(kin));
+         outputFile = new File(getOutFile(kin, inputFile));
+
+         //----------------------------------------------------------------System prints for tests--------------------------
 
          System.out.println("First name: " + firstName);
          System.out.println("Last name: " + lastName);
+
          System.out.println("First Int: " + int1);
          System.out.println("Second Int: " + int2);
+
+         System.out.println(inputFile.getAbsolutePath());
+         System.out.println(outputFile.getAbsolutePath());
          */
-        inputFile = new File(getFile(buf));
+
+        //------------------------write to file---------------------
+        inputFile = new File("C:\\Users\\Doomcrown\\Desktop\\Secure project\\src\\tester.txt");
+        outputFile = new File("C:\\Users\\Doomcrown\\Desktop\\Secure project\\src\\tester2.txt");
+        writeToFile("Jordan","Caraway", 10,20, inputFile, outputFile);
+
+
+
     }
 
     private static String getFirstName(BufferedReader buf) {
@@ -72,7 +87,7 @@ public class Main {
 
                 //check if name is valid with regex -> https://regexr.com/3kqol
                 if (!(Pattern.matches("^((?!.*[\\/\\^\\$\\#\\@\\!\\*\\;\\<\\>\\&\\\\]).{1,50})$", firstName))) {
-                    throw new InvalidDataException("Invalid input");
+                    throw new Exception("Invalid input");
                 }
                 break;
             } catch (Exception e) {
@@ -95,7 +110,7 @@ public class Main {
                 lastName = lastName.replaceAll("\n", ""); //remove new line
                 //check if name is valid with regex -> https://regexr.com/3kqol
                 if (!(Pattern.matches("^((?!.*[\\/\\^\\$\\#\\@\\!\\*\\;\\<\\>\\&\\\\]).{1,50})$", lastName))) {
-                    throw new InvalidDataException("Invalid input");
+                    throw new Exception("Invalid input");
                 }
                 break;
             } catch (Exception e) {
@@ -119,7 +134,7 @@ public class Main {
                 s = s.replaceAll("\u0000", ""); //Strips out null characters produced by the buffered reader
 
                 if (!isInt(s)) {
-                    throw new InvalidDataException("Invalid input");
+                    throw new Exception("Invalid input");
                 }
                 val = Integer.parseInt(s);
                 break;
@@ -145,49 +160,140 @@ public class Main {
         return true;
     }
 
-    private static String getFile(BufferedReader buf) {
+    private static String getInFile(Scanner kin)
+    {
         String s = "";
-        char[] tempInput;
         while (true) {
             try {
-                tempInput = new char[301];
-                System.out.println("Enter a valid absolute file path for input(Must exist in the working directory or a subdirectory)");
+
+                System.out.println("Enter a valid absolute file path for INPUT(Must exist in the working directory or a subdirectory)");
                 System.out.print("-File must be a java, c, or text file: ");
-                buf.read(tempInput, 0, 300);
-                s = new String(tempInput);
-                s = s.replaceAll("\n", ""); //remove new line
-                s = s.replaceAll("\u0000", ""); //Strips out null characters produced by the buffered reader
+
+                s = kin.nextLine();
 
                 if(!s.contains(System.getProperty("user.dir"))) //Checks to make sure file is at least in the working directory first
                 {
-                    throw new Exception("This path does not exist in the working directory");
+                    throw new Exception("This path does not exist in the working directory.");
                 }
 
-                if (!new File(s).exists()) //Checks if file actually exists
+                if (!new File(s).getAbsoluteFile().exists()) //Checks if file actually exists
                 {
-                    throw new FileNotFoundException("File does not exist");
+                    throw new FileNotFoundException("File does not exist.");
                 }
 
                 if(new File(s).isDirectory()) //checks if file is actually a directory
                 {
-                    throw new FileNotFoundException("Directories are not valid files");
+                    throw new FileNotFoundException("Directories are not valid files.");
                 }
 
-                if(!(s.contains(".txt") | s.contains(".java") | s.contains(".c"))) //checks to make sure file is of valid type
+                if(!(s.endsWith(".txt") | s.endsWith(".java") | s.endsWith(".c"))) //checks to make sure file is of valid type
                 {
                     throw new Exception("File must be of type .txt, .java. or .c");
                 }
-                /*
-                if(!s.contains(System.getProperty("user.dir")+"\\Main.java"))
+
+                if(s.contains(System.getProperty("user.dir")+"\\Main.java")) //checks to make sure the file is not the java source
                 {
-                    throw new Exception("File can not be this application source");
+                    throw new Exception("File can not be this application source.");
                 }
-                */
+
                 break;
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
         return s;
+    }
+
+    private static String getOutFile(Scanner kin, File inFile)
+    {
+        String s = "";
+        while (true) {
+            try {
+
+                System.out.println("Enter a valid absolute file path for OUTPUT(Must exist in the working directory or a subdirectory)");
+                System.out.print("-File must be a java, c, or text file: ");
+
+                s = kin.nextLine();
+                System.out.println(s);
+
+                if(!s.contains(System.getProperty("user.dir"))) //Checks to make sure file is at least in the working directory first
+                {
+                    throw new Exception("This path does not exist in the working directory.");
+                }
+
+                if (!new File(s).getAbsoluteFile().exists()) //Checks if file actually exists
+                {
+                    throw new FileNotFoundException("File does not exist.");
+                }
+
+                if(new File(s).isDirectory()) //checks if file is actually a directory
+                {
+                    throw new FileNotFoundException("Directories are not valid files.");
+                }
+
+                if(!(s.endsWith(".txt") | s.endsWith(".java") | s.endsWith(".c"))) //checks to make sure file is of valid type
+                {
+                    throw new Exception("File must be of type .txt, .java. or .c");
+                }
+
+                if(s.contains(System.getProperty("user.dir")+"\\Main.java")) //checks to make sure the file is not the java source
+                {
+                    throw new Exception("File can not be this application source.");
+                }
+
+                if(s.contains(inFile.getAbsolutePath())) //checks to make sure outfile is not the same as input file
+                {
+                    throw new Exception("Output file can not be the same as input file.");
+                }
+
+                break;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return s;
+    }
+
+    private static Boolean writeToFile(String fName, String lName, int int1, int int2, File inFile, File outFile)
+    {
+        String inLine;
+        PrintWriter pwOut = null;
+        BufferedReader brIn = null;
+        try {
+            FileWriter fwOut = new FileWriter(outFile, true);
+            pwOut = new PrintWriter(fwOut);
+            //Prints data for name and ints
+            pwOut.println("\n<<<<<<<<<<<<Start of File Writing>>>>>>>>>>>>");
+            pwOut.println(fName);
+            pwOut.println(lName);
+            pwOut.println((int1+int2));
+            pwOut.println((int1*int2));
+            pwOut.println();
+            FileReader fwIn = new FileReader(inFile);
+            brIn = new BufferedReader(fwIn);
+
+            while((inLine = brIn.readLine()) != null) //grabs line by line of input file and prints to output
+            {
+                pwOut.println(inLine);
+            }
+
+            pwOut.println("<<<<<<<<<<<<<End of File Writing>>>>>>>>>>>>>");
+            pwOut.println();
+
+        }catch (IOException e) {
+          e.printStackTrace();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        } finally { //for closing readers, BR needs to be ina  try block but SOP is to close streams in finally block even though try/catch auto close streams
+            try {
+                brIn.close();
+                pwOut.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        return false;
     }
 }
